aa271fe2086aecbaca452424c425f6e8d7990564
FlorianHeck2 <florianheck@mein.gmx>
2024-01-23 17:29:27 +0100

    translation

--- Main.py
+++ Main.py
@@ -3,84 +3,88 @@ import math
 import sys
 
 vector = pygame.math.Vector2
-hintergrund = pygame.image.load("images/background.png")
-fuenfeck = pygame.image.load("images/fuenfeck.png")
+background = pygame.image.load("images/background.png")
+pentagon = pygame.image.load("images/fuenfeck.png")
 screen = pygame.display.set_mode([1290,717])#Erzeugt Fenster mit Höhe und Breite in Pixeln
 clock = pygame.time.Clock()
 Go = True #Spielvariable -> solange true lädt game weiter
-Ziel=pygame.draw.rect(screen, (0,0,0), (1287, 172, 1, 64))
+Finish=pygame.draw.rect(screen, (0,0,0), (1287, 172, 1, 64))
 
 livingEnemys = [] #darin werden lebende Gegner gesichert
 spawncounter = 0 #zählt tics seit letztem spawn
 
-class Gegner:
-    def __init__(self, geschw, breite, hoehe, leben, Schaden):
-        self.position=(676-breite/2,0-hoehe/2)
-        self.vector=vector(0 ,geschw)
-        self.breite=breite
-        self.hoehe=hoehe
-        self.leben=leben
-        livingEnemys.append(self)
-        self.wegpunkt1 = pygame.draw.rect(screen,(0,0,0),(675, 227+self.breite/2, 1, 1) )
-        self.wegpunkt2 = pygame.draw.rect(screen,(0,0,0),(405-self.breite/2, 227, 1, 1) )
-        self.wegpunkt3 = pygame.draw.rect(screen,(0,0,0),(405, 107-self.hoehe/2, 1, 1) )
-        self.wegpunkt4 = pygame.draw.rect(screen,(0,0,0),(188-self.breite/2, 107, 1, 1) )
-        self.wegpunkt5 = pygame.draw.rect(screen,(0,0,0),(188, 492+self.hoehe/2, 1, 1) )
-        self.wegpunkt6 = pygame.draw.rect(screen,(0,0,0),(969+self.breite/2, 492, 1, 1) )
-        self.wegpunkt7 = pygame.draw.rect(screen,(0,0,0),(969, 204-self.hoehe/2, 1, 1) )
+class Player:
+    def __init__(self, Health):
+        self.Health = Health
 
-        self.figur = pygame.draw.rect(screen, (0,0,0),(676-(self.breite/2), 0-(self.hoehe/2), self.breite, self.hoehe))   
+class Enemy:
+    def __init__(self, speed, width, height, health, Schaden):
+        self.position=(676-width/2,0-height/2)
+        self.vector=vector(0 ,speed)
+        self.width=width
+        self.height=height
+        self.health=health
+        livingEnemys.append(self)
+        self.waypoint1 = pygame.draw.rect(screen,(0,0,0),(675, 227+self.width/2, 1, 1) )
+        self.waypoint2 = pygame.draw.rect(screen,(0,0,0),(405-self.width/2, 227, 1, 1) )
+        self.waypoint3 = pygame.draw.rect(screen,(0,0,0),(405, 107-self.height/2, 1, 1) )
+        self.waypoint4 = pygame.draw.rect(screen,(0,0,0),(188-self.width/2, 107, 1, 1) )
+        self.waypoint5 = pygame.draw.rect(screen,(0,0,0),(188, 492+self.height/2, 1, 1) )
+        self.waypoint6 = pygame.draw.rect(screen,(0,0,0),(969+self.width/2, 492, 1, 1) )
+        self.waypoint7 = pygame.draw.rect(screen,(0,0,0),(969, 204-self.height/2, 1, 1) )
+
+        self.figur = pygame.draw.rect(screen, (0,0,0),(676-(self.width/2), 0-(self.height/2), self.width, self.height))   
         
 
     def EnemyZeichnen(self):
-        self.figur = pygame.draw.rect(screen, (0,0,0),(self.position[0], self.position[1], self.breite, self.hoehe))   
+        self.figur = pygame.draw.rect(screen, (0,0,0),(self.position[0], self.position[1], self.width, self.height))   
         
     def Laufen(self):
         self.position += self.vector
-        if self.figur.colliderect(self.wegpunkt1):
+        if self.figur.colliderect(self.waypoint1):
             self.vector.rotate_ip(90)
-            self.position=(675-self.breite/2,227-self.hoehe/2)   
-            self.wegpunkt1 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt2):
+            self.position=(675-self.width/2,227-self.height/2)   
+            self.waypoint1 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint2):
             self.vector.rotate_ip(90)
-            self.position=(405-self.breite/2,227-self.hoehe/2)   
-            self.wegpunkt2 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt3):
+            self.position=(405-self.width/2,227-self.height/2)   
+            self.waypoint2 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint3):
             self.vector.rotate_ip(-90)
-            self.position=(405-self.breite/2,103-self.hoehe/2)   
-            self.wegpunkt3 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt4):
+            self.position=(405-self.width/2,103-self.height/2)   
+            self.waypoint3 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint4):
             self.vector.rotate_ip(-90)
-            self.position=(188-self.breite/2,103-self.hoehe/2)   
-            self.wegpunkt4 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt5):
+            self.position=(188-self.width/2,103-self.height/2)   
+            self.waypoint4 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint5):
             self.vector.rotate_ip(-90)
-            self.position=(188-self.breite/2,492-self.hoehe/2)   
-            self.wegpunkt5 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt6):
+            self.position=(188-self.width/2,492-self.height/2)   
+            self.waypoint5 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint6):
             self.vector.rotate_ip(-90)
-            self.position=(969-self.breite/2,492-self.hoehe/2)   
-            self.wegpunkt6 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
-        elif self.figur.colliderect(self.wegpunkt7):
+            self.position=(969-self.width/2,492-self.height/2)   
+            self.waypoint6 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+        elif self.figur.colliderect(self.waypoint7):
             self.vector.rotate_ip(90)
-            self.position=(969-self.breite/2,204-self.hoehe/2)   
-            self.wegpunkt7 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
+            self.position=(969-self.width/2,204-self.height/2)   
+            self.waypoint7 = pygame.draw.rect(screen,(0,0,0),(-10, -10, 1, 1) )
 
-        if self.figur.colliderect(Ziel):
+        if self.figur.colliderect(Finish):
           livingEnemys.remove(self)
-          print(len(livingEnemys))
+          print(len(livingEnemys))#Kontrolle
          
                
 
-Enemy1 = Gegner(6, 30, 30,10, 1)
+Enemy1 = Enemy(6, 30, 30,10, 1)
 while Go:
     if spawncounter == 20:
-         Enemy1 = Gegner(6, 30, 30,10, 1)
+         Enemy1 = Enemy(6, 30, 30,10, 1)
          spawncounter = 0
          
     for event in pygame.event.get():#Tastatur/Spielefenstereingaben abgreifen
         if event.type ==pygame.QUIT: sys.exit()#Spiel schließen
-    screen.blit(hintergrund, (0,0))
+    screen.blit(background, (0,0))
     for Enemys in livingEnemys:
          Enemys.Laufen()
          Enemys.EnemyZeichnen()
